cb638a61cafa56e45a41f387dd7058dbc4bf3f46
Andrey <vosirandr@gmail.com>
2023-09-22 18:19:44 +0300

    add committed

---
+++ 02_global.js
@@ -0,0 +1,21 @@
+// console.log(global);
+
+// setTimeout(() => {
+//   console.log('Hello!');
+// }, 2000);
+
+// console.log(__dirname);
+// console.log(__filename);
+
+// console.log(process);
+// console.log(process.env);
+
+// console.log(process.argv);
+// console.log(`Hello, ${process.argv[2]}`);
+// console.log(typeof process.argv);
+
+const url = new URL('https://webDev.com/path/name');
+console.log(url.hostname);
+console.log(url.href);
+console.log(url.pathname);
+console.log(url.hash)
