977c1532e155a4f35250e4503acc77ae957670af
Andrey <vosirandr@gmail.com>
2023-09-25 16:44:10 +0300

    add code

---
+++ 06_buffer_stream/app.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const zlib = require('zlib')
+
+const readStream = fs.createReadStream('./docs/text.txt')
+const writeStream = fs.createWriteStream('./docs/new-text.txt');
+const compressStream = zlib.createGzip();
+
+// readStream.on('data', (chunk) => {
+//   writeStream.write('\n---CHUNK START---\n');
+//   writeStream.write(chunk)
+//   writeStream.write('\n---CHUNK END---\n');
+// })
+
+const handleError = () => {
+  console.log('Error');
+  readStream.destroy();
+  writeStream.end('Finished with error...');
+}
+
+readStream
+  .on('error', handleError)
+  .pipe(compressStream)
+  .pipe(writeStream)
+  .on('error', handleError);
