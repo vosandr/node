9ddf6862ec86dbda8dbf0352b0138a81ff593c3e
Andrey <vosirandr@gmail.com>
2023-09-29 19:45:02 +0300

    add code

--- src/08_createServer/app.ts
+++ src/08_createServer/app.ts
@@ -3,12 +3,24 @@ const http = require('http');
 const PORT = 3000;
 
 const server = http.createServer((req: any, res: any) =>{
-  console.log('Server request');
+  console.log('Server request')
   console.log(req.url, req.method);
+
+  res.setHeader('Content-Type', 'application/json')
+  // res.write('<head><link rel="stylesheet" href="#"></head>')
+  // res.write('<h1>Hello world!</h1>');
+
+  const data = JSON.stringify([
+    { name: 'Tommy', age: 35},
+    { name: 'Arthur', age: 40},
+  ])
+  res.end(data);
 });
 
 
 
 server.listen(PORT, 'localhost', (error: any) => {
-  error ? console.log(error) : console.log(`listening port ${PORT}`);
+  const outSide = [error, `listening port ${PORT}`];
+
+  console.log(outSide[0|1]);
 })
